19300f96ec6b1d5ee364b6c5c49f1748a3292087
Abdullah Shakeel <abdulai9023@gmail.com>
2026-07-10 17:53:31 +0500

    Add files via upload

---
+++ Pyhton-labs/Data_types_and_variables_lab/variables_lab.py
@@ -0,0 +1,14 @@
+## Creating variables of diff data types
+
+name = "Jhon"  #String
+age = 22  #Integer
+is_student = True #Bolean
+height = 175.4  #Float
+
+
+## Printing variables with  Data types
+
+print("Name:", name, "Type", type(name))
+print("Age:", age, "Type:", type(age))
+print("Is Student:", is_student, "Type:", type(is_student))
+print("Height", height, "Type", type(height))
