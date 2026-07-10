29379d4cd5de3590d5ba9aeac25a5d0561d387ae
Abdullah Shakeel <abdulai9023@gmail.com>
2026-07-10 22:29:44 +0500

    Add files via upload

---
+++ Pyhton-labs/Arithmatic_Expressions/arith_op.py
@@ -0,0 +1,29 @@
+## Input two numbers
+
+num1 = int(input("Enter num 1:"))
+num2 = int(input("Enter num 2:"))
+
+## Addition
+add_result = num1 + num2
+print(f"Addition: {num1} + {num2} = {add_result}")
+
+## Subtraction
+
+sub_result = num1 - num2
+print(f"Subtraction: {num1} - {num2} = {sub_result}")
+
+## Multiplication
+
+mul_result = num1 * num2
+print(f"Multiplication: {num1} * {num2} = {mul_result}")
+
+
+## Division
+
+div_result = num1 / num2
+print(f"Division: {num1} / {num2} = {div_result}")
+
+##Integer Division:
+
+int_div_result  = num1 // num2
+print(f"Integer Division: {num1} // {num2} = {int_div_result}")
