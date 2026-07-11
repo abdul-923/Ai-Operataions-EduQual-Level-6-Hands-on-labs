10d6d5894b05638665f6b572282089d1bfa1e216
Abdullah Shakeel <abdulai9023@gmail.com>
2026-07-11 12:02:04 +0500

    Add files via upload

---
+++ Pyhton-labs/05 - for_and_while_loop/loops.py
@@ -0,0 +1,28 @@
+
+##For Loop
+students = ["Ali", "Jhon", "Oliver", "Klevin"]
+
+for student in students:
+ print("Welcome Dear", student)
+
+
+##Another Project
+
+for number in range(1, 11):
+ answer = 5 * number
+ print("5 x", number, "=", answer)
+
+
+
+###While Loop
+
+correct_pass = "python123"
+
+password = input("Enter Your password: ")
+
+while password != correct_pass:
+ print("wrong! try again")
+ passwrod = input("enter your password: ")
+
+print("login authenticated")
+
