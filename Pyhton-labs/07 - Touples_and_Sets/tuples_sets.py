384e31e4be919ad25b6ede0f4024c4514107de58
Abdullah Shakeel <abdulai9023@gmail.com>
2026-07-13 23:29:04 +0500

    Add files via upload

---
+++ Pyhton-labs/07 - Touples_and_Sets/tuples_sets.py
@@ -0,0 +1,11 @@
+
+#Creating a touple
+
+names = ("ali", "ahmad", "java", "pyhtoin")
+
+print("names_touple:", names)
+
+##Creating Sets
+
+car = { "vigen", "bugati", "lambo"}
+print("cars_sets:", car)
