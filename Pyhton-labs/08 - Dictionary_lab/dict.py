26b3566932318b47ef646f1bee4ef35cd3004331
Abdullah Shakeel <abdulai9023@gmail.com>
2026-07-14 00:35:21 +0500

    Add files via upload

---
+++ Pyhton-labs/08 - Dictionary_lab/dict.py
@@ -0,0 +1,25 @@
+
+
+#Creat Dictionary
+
+user_profile = {
+   "name": "Alice",
+   "age": 30,
+   "city": "New York"
+}
+
+print("Dictionary:", user_profile)
+
+
+##Updating values:
+
+user_profile["age"] = 31
+
+print("updated:", user_profile)
+
+
+##Removing key
+
+user_profile.pop("city")
+
+print("removed_dict:", user_profile)
