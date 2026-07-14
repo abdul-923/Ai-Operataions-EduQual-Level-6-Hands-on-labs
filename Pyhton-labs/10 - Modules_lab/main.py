b5ca7428bf3e88d6f08e831d7052650b6b450b56
Abdullah Shakeel <abdulai9023@gmail.com>
2026-07-14 15:32:56 +0500

    Add files via upload

---
+++ Pyhton-labs/10 - Modules_lab/main.py
@@ -0,0 +1,11 @@
+
+print("##Filename: main.py")
+
+print("##Importing module from mymodules.py")
+
+import mymodules
+
+print("##calling the add function from mymodules.py")
+
+result = mymodules.add(5, 6)
+print(f"the sum:", {result})
---
+++ Pyhton-labs/10 - Modules_lab/mymodules.py
@@ -0,0 +1,12 @@
+
+
+print("##Defining Function")
+
+print("##Filename:mymodules.py")
+
+def add(a, b):
+ return a + b
+
+print("##Created another file named main.py")
+
+print("##Inport newly created module in main.py and usethe add function")
