a9c6059d87f7a6c7e90f107ed652e492de6b0336
abdul-923 <abdulai9023@gmail.com>
2026-07-16 23:24:27 +0500

    Practiced and build Comprehension lab

---
+++ Python-Labs/17 - List and Dictionary_Comprehension/comp.py
@@ -0,0 +1,13 @@
+
+
+## List Comprehension
+
+result = [x**2 for x in range(10)]
+print(result)
+
+##Dictionary Comprehension
+
+words = ["apple", "banana", "anar", "grapes"]
+
+lenghts = {i: len(i) for i in words}
+print(lenghts)
